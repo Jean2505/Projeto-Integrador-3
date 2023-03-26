6888931ded36f6cd6038ca076d76ffad524bb1a0
Joniiss <joao.ferraz.balducci@gmail.com>
2023-03-26 00:23:14 -0300

    Cadastro no Firebase pronto

---
+++ app/src/main/java/br/edu/puccampinas/pi3/Dentista.kt
@@ -0,0 +1,9 @@
+package br.edu.puccampinas.pi3
+
+data class Dentista(val nome: String?,
+                    val telefone: String?,
+                    val email: String?,
+                    val end1: String?,
+                    val end2: String?,
+                    val end3: String?,
+                    val cv: String?)
