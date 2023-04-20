1463a49e37925437081a2209db2890e6e6c20bcb
Guilherme <guidovieira2000@gmail.com>
2023-04-20 18:25:10 -0300

    Realização de login do usuário

---
+++ app/src/main/java/br/edu/puccampinas/pi3/LoadingActivity.kt
@@ -0,0 +1,43 @@
+package br.edu.puccampinas.pi3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoadingActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+
+        auth = Firebase.auth
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        Firebase.auth.signOut()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            Toast.makeText(this, "qualquer coisa que eu quiser", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            if (currentUser != null) {
+                Toast.makeText(this,currentUser.email.toString(), Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "ta muito nulo", Toast.LENGTH_SHORT).show()
+            }
+            val intentLogin = Intent(this,LoginActivity::class.java)
+
+            this.startActivity(intentLogin)
+        }
+    }
+}
