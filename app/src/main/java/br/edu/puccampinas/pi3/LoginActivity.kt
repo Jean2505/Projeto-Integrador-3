a40587c387156940240092802d1a389527dcd304
Guilherme <guidovieira2000@gmail.com>
2023-04-21 15:44:33 -0300

    Correção de Bugs

--- app/src/main/java/br/edu/puccampinas/pi3/LoginActivity.kt
+++ app/src/main/java/br/edu/puccampinas/pi3/LoginActivity.kt
@@ -3,18 +3,42 @@ package br.edu.puccampinas.pi3
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.edu.puccampinas.pi3.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = Firebase.auth
+
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnLogar.setOnClickListener {
+            auth.signInWithEmailAndPassword(
+                binding.etEmail.text.toString(),
+                binding.etSenha.text.toString()
+            )
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                    } else {
+                        Toast.makeText(
+                            baseContext, "Authentication failed." + task.exception.toString(),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
 
         binding.btnCadastrar.setOnClickListener{
             val intentCadastra = Intent(this,MainActivity::class.java)
